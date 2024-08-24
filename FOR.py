1bde0abecbbd75934779d63f4cd9b30bbd275e87
alinemorais <linianholete@gmail.com>
2024-08-24 15:42:58 -0300

    Aula 03 do Bootcamp Jornada de Dados

---
+++ FOR.py
@@ -0,0 +1,20 @@
+# %% Contagem de Palavras em Textos
+# Objetivo: Dado um texto, contar quantas vezes cada palavra única aparece nele.
+# Texto:"a raposa marrom salta sobre o cachorro preguiçoso"
+
+# %% Normalização de Dados
+# Objetivo: Normalizar uma lista de números para que fiquem na escala de 0 a 1.
+
+# %% Filtragem de Dados Faltantes
+# Objetivo: Dada uma lista de dicionários representando dados de usuários, 
+# filtrar aqueles que têm um campo específico faltando.
+
+
+# %% Extração de Subconjuntos de Dados
+# Objetivo: Dada uma lista de números, extrair apenas aqueles que são pares.
+
+# %% Agregação de Dados por Categoria
+#Objetivo: Dado um conjunto de registros de vendas, calcular 
+# o total de vendas por categoria.
+
+
---
+++ WHILE.py
@@ -0,0 +1,28 @@
+# Exemplo de Uso do while em Engenharia de Dados
+# Um cenário comum em engenharia de dados é a necessidade de 
+# executar uma tarefa de maneira periódica, como verificar novos 
+# dados em um diretório, fazer polling de uma API para novas 
+# respostas ou monitorar mudanças em um banco de dados. Nestes casos, 
+# um loop while pode ser utilizado para manter o script rodando continuamente
+# ou até que uma condição específica seja atingida (por exemplo, 
+# um sinal para desligar ou uma condição de erro).
+
+# %% Leitura de Dados até Flag
+# Objetivo: Ler dados de entrada até que uma palavra-chave específica 
+# ("sair") seja fornecida.
+
+# %% Validação de Entrada
+# Objetivo: Solicitar ao usuário um número dentro de 
+# um intervalo específico até que a entrada seja válida.
+
+# %%Consumo de API Simulado
+# Objetivo: Simular o consumo de uma API paginada, onde cada "página" d
+# e dados é processada em loop até que não haja mais páginas.
+
+# %%Tentativas de Conexão
+# Objetivo: Simular tentativas de reconexão a um serviço com um limite máximo de tentativas.
+
+# %% Processamento de Dados com Condição de Parada
+# Objetivo: Processar itens de uma lista até encontrar um
+# valor específico que indica a parada.
+
